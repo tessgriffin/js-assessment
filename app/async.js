8e359366043115fde8549d889fa750ac55443de7
Tess Griffin <tessjgriffin@gmail.com>
2015-07-05 12:51:29 -0600

    Added passing async behavior tests

--- app/async.js
+++ app/async.js
@@ -2,10 +2,18 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise(function(resolve, reject){
+      resolve(value)
+    })
   },
 
   manipulateRemoteData : function(url) {
-
+    var return_arr = [];
+    return $.get(url).then(function(people){
+      people["people"].map(function(person){
+        return_arr.push(person["name"]);
+      });
+      return return_arr.sort()
+    });
   }
 };
